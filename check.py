fddc522e5aa17a4f56eb699668a0aa8bd753d7a2
Matias <matias.heikkila@aalto.fi>
2017-09-20 13:59:49 +0300

    sd

--- check.py
+++ check.py
@@ -4,5 +4,4 @@ import numpy as np
 files = os.listdir("out")
 
 for f in files:
-    s = np.genfromtxt(os.path.join("out", f), delimiter=";")
-    print "Array in %s: %s" % (f, s.shape) 
+    print "Array in %s: %s" % (f, np.genfromtxt(os.path.join("out", f), delimiter=";").shape) 
